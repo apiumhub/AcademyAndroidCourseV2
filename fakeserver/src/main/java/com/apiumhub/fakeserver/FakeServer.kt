30ae42a14a6b370eaf864c5ef03d622a9736ce26
Diego Ojeda <diegoojedagarcia@gmail.com>
2020-01-16 11:33:38 +0100

    Extracted ktor server to a separate module

--- fakeserver/src/main/java/com/apiumhub/fakeserver/FakeServer.kt
+++ fakeserver/src/main/java/com/apiumhub/fakeserver/FakeServer.kt
@@ -1,26 +1,32 @@
 package com.apiumhub.fakeserver
 
+import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
+import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
+import io.ktor.routing.Routing
 import io.ktor.routing.get
-import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main() {
-  embeddedServer(
-      Netty,
-      port = 8080
-  ) {
-    this.install(ContentNegotiation) { gson {} }
-    routing {
-      get("/status") { call.respond(HttpStatusCode.OK, Test()) }
+  embeddedServer(Netty, 8080, watchPaths = listOf("FakeServerKt"), module = Application::module).start()
+}
+
+fun Application.module() {
+  install(DefaultHeaders)
+  install(CallLogging)
+  install(ContentNegotiation) { gson {} }
+  install(Routing) {
+    get("/status") {
+      call.respond(HttpStatusCode.OK, Test())
     }
-  }.start()
+  }
 }
 
 data class Test(
