2ebf0d06c2d72a583610fd3d90c71f6b7a045e23
Diego <diego.ojeda@apiumhub.com>
2020-03-21 19:10:26 +0100

    Add mockk dependencies

--- grazner/src/test/java/com/apiumhub/androidcourse/timeline/domain/TimelineServiceTest.kt
+++ grazner/src/test/java/com/apiumhub/androidcourse/timeline/domain/TimelineServiceTest.kt
@@ -1,17 +1,34 @@
 package com.apiumhub.androidcourse.timeline.domain
 
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
 
 class TimelineServiceTest {
-
   private val repository = mockk<TimelineRepository>()
   private val sut = TimelineService(repository)
 
   @Test
-  fun getTimeline() {
+  fun getTimeline() = runBlockingTest {
+    Dispatchers.setMain()
+    coEvery {
+
+    }
+    coVerify {
+
+    }
 //    TODO()
   }
 }
